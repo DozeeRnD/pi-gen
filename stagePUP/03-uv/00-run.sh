7a558c98f33d9cee3181b614a330ca4e8a919ca2
Umesh Mohan <umesh.mohan@dozee.io>
2024-09-18 15:10:55 +0530

    install uv from their install script

--- stagePUP/03-uv/00-run.sh
+++ stagePUP/03-uv/00-run.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-install -v -m 0755 -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local"
-install -v -m 0755 -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/bin"
-install -v -m 755 -o 1000 -g 1000  files/uv "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.local/bin"
+on_chroot << EOF
+    su -c "curl -LsSf https://astral.sh/uv/install.sh | sh" ${FIRST_USER_NAME}
+EOF
