da04c2a755a1a5710a89c3c105a8ef8edaea4bf0
Umesh Mohan <umesh.mohan@dozee.io>
2024-09-18 15:43:24 +0530

    fish_add_path -m flag to prevent errors

--- stagePUP/02-fish-shell-default/00-run.sh
+++ stagePUP/02-fish-shell-default/00-run.sh
@@ -3,6 +3,6 @@
 set -x
 
 on_chroot << EOF
-chsh -s /usr/bin/fish ${FIRST_USER_NAME}
-su -c "fish -c \"fish_add_path /home/${FIRST_USER_NAME}/.local/bin\"" ${FIRST_USER_NAME}
+    chsh -s /usr/bin/fish ${FIRST_USER_NAME}
+    su -c "fish -c \"fish_add_path /home/${FIRST_USER_NAME}/.local/bin -m\"" ${FIRST_USER_NAME}
 EOF
